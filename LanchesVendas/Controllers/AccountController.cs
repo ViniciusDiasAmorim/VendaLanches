d36ec83766ea3e984995ca8428a5c1d07101dae5
ViniciusDiasAmorim <100163241+ViniciusDiasAmorim@users.noreply.github.com>
2022-06-08 19:04:48 -0300

    Adicionando AccountController realizando logica e criando View Login

---
+++ LanchesVendas/Controllers/AccountController.cs
@@ -0,0 +1,53 @@
+﻿using LanchesVendas.ViewModel;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace LanchesVendas.Controllers
+{
+    public class AccountController : Controller
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+        private readonly SignInManager<IdentityUser> _signInManager;
+
+        public AccountController(UserManager<IdentityUser> userManager, SignInManager<IdentityUser> signInManager)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+        }
+
+        public IActionResult Login(string returnUrl)
+        {
+            return View(new LoginViewModel()
+            {
+                ReturnUrl = returnUrl
+            });
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Login(LoginViewModel login)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(login);
+            }
+            var user = await _userManager.FindByNameAsync(login.Usuario);
+
+            if (user != null)
+            {
+                var result = await _signInManager.PasswordSignInAsync(user, login.Senha, false, false);
+                
+                if(result.Succeeded)
+                {
+                    if(string.IsNullOrEmpty(login.ReturnUrl))
+                    {
+                        return RedirectToAction("Index", "Home");
+                    }
+                    return Redirect(login.ReturnUrl);
+                }
+            }
+            ModelState.AddModelError("", "Erro ao efetuar login");
+            return View(login);
+        }
+
+    }
+}
---
+++ LanchesVendas/ViewModel/LoginViewModel.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations;
+
+namespace LanchesVendas.ViewModel
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "Infome o nome de usuario")]
+        public string Usuario { get; set; }
+        [Required(ErrorMessage = "Infome a senha")]
+        [DataType(DataType.Password)]
+        public string Senha { get; set; }
+        [ValidateNever]
+        public string ReturnUrl { get; set; }
+    }
+}
